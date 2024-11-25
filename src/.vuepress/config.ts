bf41a86d1bffca910ecf7432a077b21bed5db7e3
Carmen <2942173241@qq.com>
2024-11-25 16:35:20 +0800

    鼠标点击随机显示词语

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,13 +4,25 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/painlife/",
-
   lang: "zh-CN",
   title: "\"极乐\"人生",
   description: "\"极乐\"人生",
-
+  head: [
+    // 引入jquery
+    ["script", {
+      "language": "javascript",
+      "type": "text/javascript",
+      "src": "https://code.jquery.com/jquery-3.7.1.js",
+      "defer": "true"
+    }],
+    // 引入鼠标点击脚本
+    ["script", {
+      "language": "javascript",
+      "type": "text/javascript",
+      "src": "/painlife/js/mouseClickEffect.js",
+      "defer": "true"
+    }]
+  ],
   theme,
-
-  // 和 PWA 一起启用
-  // shouldPrefetch: false,
 });
+
